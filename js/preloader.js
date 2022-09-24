031913636dddf7ae17c892a988ae8bfa35a48d3a
kinyapina <kinyapina.katya28@gmail.com>
2022-09-24 20:56:22 +0700

    added project after 2 lessons

---
+++ js/preloader.js
@@ -0,0 +1,10 @@
+const preloader = () => {
+    const preloader = document.querySelector('.preloder');
+    preloader.classList.add('active');
+
+    setTimeout(() => {
+        preloader.classList.remove('active')
+    }, 500);
+}
+
+preloader();
